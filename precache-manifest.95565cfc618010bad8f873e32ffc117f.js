4ffaa2921d7df1f507801540a0c44b7f1dbfd743
loardjavad <javad.farmhc@gmail.com>
2019-12-04 21:46:05 +0330

    Updates

---
+++ precache-manifest.95565cfc618010bad8f873e32ffc117f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c55d910da5b469645243f4a0b92e07",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7c42cd03dae8959623f0",
+    "url": "/monsters-rolodex/static/css/main.426b2cd3.chunk.css"
+  },
+  {
+    "revision": "858b8215468bbd49473b",
+    "url": "/monsters-rolodex/static/js/2.00222684.chunk.js"
+  },
+  {
+    "revision": "7c42cd03dae8959623f0",
+    "url": "/monsters-rolodex/static/js/main.8d0d813d.chunk.js"
+  },
+  {
+    "revision": "075343449241dbe0ad6c",
+    "url": "/monsters-rolodex/static/js/runtime-main.58539e12.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/monsters-rolodex/static/media/logo.25bf045c.svg"
+  }
+]);
